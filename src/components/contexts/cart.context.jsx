39b174ef0510f0d53cc2e63142402b58899fc3ad
agam.j@iitgn.ac.in <agam.j@iitgn.ac.in>
2022-07-10 02:24:13 +0530

    added reducer in context component

--- src/components/contexts/cart.context.jsx
+++ src/components/contexts/cart.context.jsx
@@ -1,5 +1,6 @@
-import { useEffect } from "react";
-import { useState, createContext } from "react";
+
+import {  createContext, useReducer } from "react";
+import {createAction} from './../../utils/reducer/reducer.utils.js'
 
 const addCartItme = (cartItems, productToAdd) => {
     const existingCartItem = cartItems.find(
@@ -14,26 +15,28 @@ const addCartItme = (cartItems, productToAdd) => {
     }
     return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
-const removeCartItem=(cartItems,cartItemToRemove)=>{
+const removeCartItem = (cartItems, cartItemToRemove) => {
     //fidn the cart item to remove
     const existingCartItem = cartItems.find(
         (cartItems) => cartItems.id === cartItemToRemove.id
     );
     //check if quantity is 1
-    if(existingCartItem.quantity==1){
-        return cartItems.filter(cartItem=>cartItem.id!==cartItemToRemove.id)
+    if (existingCartItem.quantity == 1) {
+        return cartItems.filter(
+            (cartItem) => cartItem.id !== cartItemToRemove.id
+        );
     }
     return cartItems.map((cartItem) =>
-    cartItem.id === cartItemToRemove.id
-        ? { ...cartItem, quantity: cartItem.quantity - 1 }
-        : cartItem
-);
+        cartItem.id === cartItemToRemove.id
+            ? { ...cartItem, quantity: cartItem.quantity - 1 }
+            : cartItem
+    );
 
     //return bakc car
-}
-const clearCartItem=(cartItems,cartItemToClear)=>{
-    return cartItems.filter(cartItem=>cartItem.id!==cartItemToClear.id)
-}
+};
+const clearCartItem = (cartItems, cartItemToClear) => {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+};
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
@@ -41,39 +44,73 @@ export const CartContext = createContext({
     removeItemToCart: () => {},
     addItemToCart: () => {},
     cartCount: 0,
-    clearItemFromCart:()=>{},
-    cartTotal:0
+    clearItemFromCart: () => {},
+    cartTotal: 0,
 });
+const CART_ACTION_TYPES={
+    SET_CART_ITEMS:"SET_CART_ITEMS",
+    SET_IS_CART_OPEN:"SET_IS_CART_OPEN"
 
+}
+const INTIAL_STATE = {
+    cartTotal: 0,
+    isCartOpen: false,
+    cartItems: [],
+    cartCount: 0,
+};
+const cartReducer = (state, action) => {
+    const { type, payload } = action;
+    switch (type) {
+        case CART_ACTION_TYPES.SET_CART_ITEMS:
+            return {
+                ...state,
+                ...payload,
+            };
+
+        case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+            return {
+                ...state,
+                isCartOpen:payload
+            }
+        default:
+            throw new Error(`unHanderldtype of ${type} in cartReducer`);
+    }
+};
+
+const AddTOCartAction = () => {};
 export const CartProvider = ({ children }) => {
-    const [isCartOpen, setIsCartOpen] = useState(false);
-
-    const [cartItems, setCartItems] = useState([]);
-    const [cartCount, setCartCount] = useState(0);
-    const [cartTotal,setCartTotal]=useState(0);
-    useEffect(() => {
-        const newCartCount = cartItems.reduce(
+    const [{ cartTotal, isCartOpen, cartItems, cartCount }, dispatch] =
+        useReducer(cartReducer, INTIAL_STATE);
+    const updateCartItemsReducer = (newCartItems) => {
+        const newCartCount = newCartItems.reduce(
             (total, cartItem) => total + cartItem.quantity,
             0
         );
-        setCartCount(newCartCount);
-    }, [cartItems]);
-    useEffect(() => {
-        const newCartTotal = cartItems.reduce(
-            (total, cartItem) => total + cartItem.quantity* cartItem.price,
+        const newCartTotal = newCartItems.reduce(
+            (total, cartItem) => total + cartItem.quantity * cartItem.price,
             0
         );
-        setCartTotal(newCartTotal);
-    }, [cartItems]);
+        dispatch(createAction(CART_ACTION_TYPES.SET_CART_ITEMS, {
+            cartItems: newCartItems,
+            cartTotal: newCartTotal,
+            cartCount: newCartCount,
+        }));
+    };
     const addItemToCart = (productToAdd) => {
-        setCartItems(addCartItme(cartItems, productToAdd));
+        const newCartItems = addCartItme(cartItems, productToAdd);
+        updateCartItemsReducer(newCartItems);
     };
     const removeItemToCart = (cartItemToRemove) => {
-        setCartItems(removeCartItem(cartItems, cartItemToRemove));
+        const newCartItems = removeCartItem(cartItems, cartItemToRemove);
+        updateCartItemsReducer(newCartItems);
     };
     const clearItemFromCart = (cartItemToClear) => {
-        setCartItems(clearCartItem(cartItems, cartItemToClear));
+        const newCartItems = clearCartItem(cartItems, cartItemToClear);
+        updateCartItemsReducer(newCartItems);
     };
+    const setIsCartOpen=(bool)=>{
+        dispatch(createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN,bool))
+    }
     const value = {
         isCartOpen,
         setIsCartOpen,
@@ -82,8 +119,7 @@ export const CartProvider = ({ children }) => {
         cartCount,
         removeItemToCart,
         clearItemFromCart,
-        cartTotal
-
+        cartTotal,
     };
     return (
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
--- src/components/contexts/user.contexts.jsx
+++ src/components/contexts/user.contexts.jsx
@@ -1,19 +1,53 @@
-import { createContext,useState,useEffect } from "react";
-import {onAuthStatechangelisten,creatUserDocumentfromauth} from '../../utils/firebase.utils.js'
+import { createContext, useEffect, useReducer } from "react";
+import {
+    onAuthStatechangelisten,
+    creatUserDocumentfromauth,
+} from "../../utils/firebase.utils.js";
 //as the actual value you want to access.
-export const UserContext=createContext({
-currentUser:null,
-setCurrentUser:()=>null
-})
+export const UserContext = createContext({
+    currentUser: null,
+    setCurrentUser: () => null,
+});
 
-export const UserProvider=({children})=>{
-    const [currentUser,setCurrentUser]=useState(null)
-    const value={currentUser,setCurrentUser}
-    useEffect(()=>{
-        const unsubscribe=onAuthStatechangelisten((user)=>{
-            if(user){ creatUserDocumentfromauth(user);}
-            setCurrentUser(user)})
+//here we store the object
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+const userReducer = (state, action) => {
+    console.log('dispatch')
+    console.log(action)
+    const { type, payload } = action;
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload,
+            };
+      
+        default:
+            throw new Error("unhandled type with user reducer");
+    }
+};
+const INTIAL_STATE = {
+    currentUser: null,
+};
+export const UserProvider = ({ children }) => {
+    const [{ currentUser }, dispatch] = useReducer(userReducer, INTIAL_STATE);
+    console.log(currentUser)
+    const setCurrentUser = (user) => {
+        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    };
+    const value = { currentUser, setCurrentUser };
+    useEffect(() => {
+        const unsubscribe = onAuthStatechangelisten((user) => {
+            if (user) {
+                creatUserDocumentfromauth(user);
+            }
+            setCurrentUser(user);
+        });
         return unsubscribe;
-    },[])
-    return <UserContext.Provider value={value} >{children}</UserContext.Provider>
-}
+    }, []);
+    return (
+        <UserContext.Provider value={value}>{children}</UserContext.Provider>
+    );
+};
